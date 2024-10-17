0a9d47419c1bebed871c8bc5687a0b4a5f2d3588
Diego-90-Garcia <diego_1990garcia@outlook.com>
2024-10-17 20:14:10 -0300

    primeiro commit

---
+++ teste.js
@@ -0,0 +1,37 @@
+//let heroi = ('Digite o nome do herói: ');
+//let exp = ('Digite seu nível de experiencia: ');
+
+let heroi = ("diego");
+let exp = ("9000");
+
+//while (heroi === '' || exp === '') {
+    //heroi = prompt('Digite o nome do herói: ');
+    //exp = prompt('Digite seu nível de experiencia: ');
+//}
+
+exp = parseInt(exp);
+let nivel = "";
+const extra = ("É de mais de 8 MIL!!!!");
+
+if (exp <= 1000) {
+    nivel = "Ferro";
+} else if (exp <= 2000) {
+    nivel = "Bronze";
+} else if (exp <= 5000) {
+    nivel = "Prata";
+} else if (exp <= 7000) {
+    nivel = "Ouro";
+} else if (exp <= 8000) {
+    nivel = "Platina";
+} else if (exp <= 9000) {
+    nivel = "Ascendente";
+} else if (exp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log("O Herói de nome **" + heroi +"** está no nível de **" + nivel + "**.");
+if (exp > 8000) {
+    console.log(extra);
+}
